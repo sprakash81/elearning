4940e49f6263d05b662a080e97d5f664957a283c
Shyam Prakash <prakash.shyam@gmail.com>
2015-03-05 10:11:47 +1100

    Changes to sql scripts
    Updated the insert and drop scripts, after create scripts were changed
    yesterday.

--- sql/02 Insert Data.sql
+++ sql/02 Insert Data.sql
@@ -1,8 +1,8 @@
 USE elearning;
 
-insert into address values (1,'6-10 Railway Pde','Westmead','NSW','Australia','2145',0);
+insert into account values (1,'Shyam','Prakash','prakash.shyam@gmail.com',1,0);
 
-insert into account values (1,'Shyam','Prakash','prakash.shyam@gmail.com',1,1,0);
+insert into address values (1,1,'6-10 Railway Pde','Westmead','NSW','Australia','2145',0);
 
 insert into course values (1, 'CRSE01', 'Mathematics',1,0);
 insert into course values (2, 'CRSE02', 'Physics',1,0);
--- sql/03 Drop Tables.sql
+++ sql/03 Drop Tables.sql
@@ -1,6 +1,6 @@
 USE elearning;
 
 drop table training;		
+drop table address;
 drop table course;
 drop table account;
-drop table address;
